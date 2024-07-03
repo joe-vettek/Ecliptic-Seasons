254a5d30c62ed23f8db8203767374e49bbfa1176
joe-vettek <my1233215@126.com>
2024-07-03 14:45:54 +0800

    Create CommandHandler.java

---
+++ src/main/java/xueluoanping/ecliptic/handler/CommandHandler.java
@@ -0,0 +1,66 @@
+package xueluoanping.ecliptic.handler;
+
+import cloud.lemonslice.teastory.capability.CapabilitySolarTermTime;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.ResourceLocationArgument;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import xueluoanping.ecliptic.Ecliptic;
+
+@Mod.EventBusSubscriber(modid = Ecliptic.MODID)
+public class CommandHandler {
+    @SubscribeEvent
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        var dispatcher = event.getDispatcher();
+        dispatcher.register(Commands.literal("solar").requires((source) -> source.hasPermission(2))
+                .then(Commands.literal("set").then(Commands.argument("day", IntegerArgumentType.integer()).executes(commandContext -> setDay(commandContext.getSource(), IntegerArgumentType.getInteger(commandContext, "day")))))
+                .then(Commands.literal("add").then(Commands.argument("day", IntegerArgumentType.integer()).executes(commandContext -> addDay(commandContext.getSource(), IntegerArgumentType.getInteger(commandContext, "day"))))));
+    }
+
+
+    private static int getDay(ServerLevel worldIn)
+    {
+        return worldIn.getCapability(CapabilitySolarTermTime.WORLD_SOLAR_TIME).map(CapabilitySolarTermTime.Data::getSolarTermsDay).orElse(0);
+    }
+
+    public static int setDay(CommandSourceStack source, int day)
+    {
+        for (ServerLevel ServerLevel : source.getServer().getAllLevels())
+        {
+            ServerLevel.getCapability(CapabilitySolarTermTime.WORLD_SOLAR_TIME).ifPresent(data ->
+            {
+                data.setSolarTermsDay(day);
+                data.sendUpdateMessage(ServerLevel);
+            });
+        }
+
+        source.sendSuccess(()->Component.translatable("commands.teastory.solar.set", day), true);
+        return getDay(source.getLevel());
+    }
+
+    public static int addDay(CommandSourceStack source, int add)
+    {
+        for (ServerLevel ServerLevel : source.getServer().getAllLevels())
+        {
+            ServerLevel.getCapability(CapabilitySolarTermTime.WORLD_SOLAR_TIME).ifPresent(data ->
+            {
+                data.setSolarTermsDay(data.getSolarTermsDay() + add);
+                data.sendUpdateMessage(ServerLevel);
+                source.sendSuccess(()->Component.translatable("commands.teastory.solar.set", data.getSolarTermsDay()), true);
+            });
+        }
+        return getDay(source.getLevel());
+    }
+}
