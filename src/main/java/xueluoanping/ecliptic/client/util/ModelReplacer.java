dc22456259f22d9f822e3378b46a5c9e362dcc21
joe-vettek <my1233215@126.com>
2024-07-06 23:14:36 +0800

    fix some

--- src/main/java/xueluoanping/ecliptic/client/util/ModelReplacer.java
+++ src/main/java/xueluoanping/ecliptic/client/util/ModelReplacer.java
@@ -1,13 +1,10 @@
 package xueluoanping.ecliptic.client.util;
 
-import cloud.lemonslice.teastory.capability.CapabilitySolarTermTime;
-import cloud.lemonslice.teastory.environment.solar.Season;
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.BlockModelShaper;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
@@ -17,15 +14,14 @@ import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.fml.loading.FMLLoader;
 import xueluoanping.ecliptic.Ecliptic;
 import xueluoanping.ecliptic.client.ClientSetup;
 import xueluoanping.ecliptic.util.SolarUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ModelReplacer {
     // if (state.getBlock() instanceof GrassBlock&& blockAndTintGetter.getBlockState(pos.above()).isAir()
@@ -69,37 +65,67 @@ public class ModelReplacer {
         return outBakedQuads;
     }
 
+
+    // TODO:内存更新，双链表+Hash，用LRU
+    public static Map<Long, Boolean> blockMap = new ConcurrentHashMap<>();
+    public static Map<List<BakedQuad>, List<BakedQuad>> quadMap = new ConcurrentHashMap<>();
+
+
+    // 实际上这里之所以太慢还有个问题就是会一个方块访问七次
     public static List<BakedQuad> appendOverlay(BlockAndTintGetter blockAndTintGetter, BlockState state, BlockPos pos, Direction direction, RandomSource random, List<BakedQuad> list) {
         // Minecraft.getInstance().level.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING,pos);
-        if (direction != null && direction != Direction.DOWN) {
-            if (getLight(blockAndTintGetter, pos, random) >= blockAndTintGetter.getMaxLightLevel()) {
-                BakedModel snowModel = null;
-                BlockState snowState = null;
-                if (ClientSetup.snowOverlayBlock.resolve().isPresent() && (state.isSolidRender(blockAndTintGetter, pos)
-                        || state.is(BlockTags.LEAVES)
-                        || (state.getBlock() instanceof SlabBlock && state.getValue(SlabBlock.TYPE) == SlabType.TOP))) {
-                    snowModel=ClientSetup.snowOverlayBlock.resolve().get();
-                } else if (ClientSetup.snowySlabBottom.resolve().isPresent() && state.getBlock() instanceof SlabBlock) {
-                    list = new ArrayList<>(list);
-                    snowModel=ClientSetup.snowySlabBottom.resolve().get();
-                } else if (ClientSetup.models != null && state.getBlock() instanceof StairBlock) {
-                    snowState = Ecliptic.ModContents.snowyStairs.get().defaultBlockState()
-                            .setValue(StairBlock.FACING, state.getValue(StairBlock.FACING))
-                            .setValue(StairBlock.HALF, state.getValue(StairBlock.HALF))
-                            .setValue(StairBlock.SHAPE, state.getValue(StairBlock.SHAPE));
-                    // ClientSetup.models.get(new ModelResourceLocation(Ecliptic.ModContents.snowyStairs.getId(),"facing=north,half=bottom,shape=outer_left,waterlogged=true"))
-                    snowModel = ClientSetup.models.get(BlockModelShaper.stateToModelLocation(snowState));
-                }
-                if (snowModel != null) {
-                    int size = list.size();
-                    var snowList = snowModel.getQuads(snowState, direction, null);
-                    var newList = new ArrayList<BakedQuad>(size + snowList.size());
-                    newList.addAll(list);
-                    newList.addAll(snowList);
-                    list = newList;
+        // 不处理空列表，这代表着不处理这个方向
+        if (direction != Direction.DOWN &&!list.isEmpty()) {
+            boolean isLight = false;
+            long blockLong = pos.asLong();
+            if (blockMap.containsKey(blockLong)) {
+                isLight = blockMap.get(pos.asLong());
+            } else {
+                if (Minecraft.getInstance().level.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, pos).asLong() != pos.above().asLong())
+                    return list;
+                boolean checkIsLight = getLight(blockAndTintGetter, pos, state, random) >= blockAndTintGetter.getMaxLightLevel();
+                isLight = checkIsLight;
+                blockMap.put(blockLong, checkIsLight);
+            }
+            if (blockMap.size()>50000){
+
+            }
+
+            if (isLight) {
+                if (quadMap.containsKey(list)) {
+                    return quadMap.get(list);
+                } else {
+                    BakedModel snowModel = null;
+                    BlockState snowState = null;
+                    if (ClientSetup.snowOverlayBlock.resolve().isPresent() && (state.isSolidRender(blockAndTintGetter, pos)
+                            || state.is(BlockTags.LEAVES)
+                            || (state.getBlock() instanceof SlabBlock && state.getValue(SlabBlock.TYPE) == SlabType.TOP))) {
+                        snowModel = ClientSetup.snowOverlayBlock.resolve().get();
+                    } else if (ClientSetup.snowySlabBottom.resolve().isPresent() && state.getBlock() instanceof SlabBlock) {
+                        list = new ArrayList<>(list);
+                        snowModel = ClientSetup.snowySlabBottom.resolve().get();
+                    } else if (ClientSetup.models != null && state.getBlock() instanceof StairBlock) {
+                        snowState = Ecliptic.ModContents.snowyStairs.get().defaultBlockState()
+                                .setValue(StairBlock.FACING, state.getValue(StairBlock.FACING))
+                                .setValue(StairBlock.HALF, state.getValue(StairBlock.HALF))
+                                .setValue(StairBlock.SHAPE, state.getValue(StairBlock.SHAPE));
+                        // ClientSetup.models.get(new ModelResourceLocation(Ecliptic.ModContents.snowyStairs.getId(),"facing=north,half=bottom,shape=outer_left,waterlogged=true"))
+                        // 楼梯的方向是无
+                        snowModel = ClientSetup.models.get(BlockModelShaper.stateToModelLocation(snowState));
+                    }
+                    if (snowModel != null) {
+                        int size = list.size();
+                        var snowList = snowModel.getQuads(snowState, direction, null);
+                        var newList = new ArrayList<BakedQuad>(size + snowList.size());
+                        newList.addAll(list);
+                        newList.addAll(snowList);
+                        quadMap.put(list, newList);
+                        list = newList;
+                    }
                 }
             }
 
+            // var time=System.currentTimeMillis();
             // 性能测试
             // if (ClientSetup.snowOverlayBlock.resolve().isPresent() && (state.isSolidRender(blockAndTintGetter, pos)
             //         || state.is(BlockTags.LEAVES)
@@ -123,7 +149,7 @@ public class ModelReplacer {
     }
 
 
-    public static int getLight(BlockAndTintGetter blockAndTintGetter, BlockPos pos, RandomSource random) {
+    public static int getLight(BlockAndTintGetter blockAndTintGetter, BlockPos pos, BlockState state, RandomSource random) {
         // Ecliptic.logger(pos);
         int result = 0;
         if (FMLLoader.getLoadingModList().getModFileById("embeddium") != null) {
@@ -136,7 +162,7 @@ public class ModelReplacer {
         }
         result += blockAndTintGetter.getBlockState(pos.above()).is(BlockTags.SNOW) ? 15 : 0;
         // TODO:这里等会传Blocksate进来
-        // result += blockAndTintGetter.getBlockState(pos).is(BlockTags.SNOW) ? -100 : 0;
+        result += state.is(BlockTags.SNOW) ? -100 : 0;
         // result += SolarUtil.getProvider(Minecraft.getInstance().level).getSolarTerm().getSeason() == Season.WINTER ? 0 : -100;
         if (result >= 15) {
             // TODO:未来设置为降雨
