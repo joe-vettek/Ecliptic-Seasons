ab11f4a60eb53254aaa9d14cc18ce429c37a7515
joe-vettek <my1233215@126.com>
2024-08-01 13:08:00 +0800

    fix panda crash

--- src/main/java/com/teamtea/ecliptic/mixin/client/MixinClientPanda.java
+++ src/main/java/com/teamtea/ecliptic/mixin/client/MixinClientPanda.java
@@ -1,4 +1,4 @@
-package com.teamtea.ecliptic.mixin.common.entity.animal.pandas;
+package com.teamtea.ecliptic.mixin.client;
 
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -7,22 +7,21 @@ import com.teamtea.ecliptic.client.core.ClientWeatherChecker;
 import com.teamtea.ecliptic.common.core.biome.WeatherManager;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.animal.Fox;
 import net.minecraft.world.entity.animal.Panda;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Panda.class)
-public class MixinPanda {
+public class MixinClientPanda {
 
     @WrapOperation(
             method = "tick",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isThundering()Z")
     )
     private boolean mixin$tick(Level instance, Operation<Boolean> original) {
-        if (instance instanceof ServerLevel serverLevel)
-            return WeatherManager.isThunderAt(serverLevel, ((Panda) (Object) this).blockPosition());
+        if (instance instanceof ClientLevel clientLevel)
+            return ClientWeatherChecker.isThunderAt(clientLevel, ((Panda) (Object) this).blockPosition());
         return original.call(instance);
     }
 
@@ -31,8 +30,8 @@ public class MixinPanda {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isThundering()Z")
     )
     private boolean mixin$isScared(Level instance, Operation<Boolean> original) {
-        if (instance instanceof ServerLevel serverLevel)
-            return WeatherManager.isThunderAt(serverLevel, ((Panda) (Object) this).blockPosition());
+        if (instance instanceof ClientLevel clientLevel)
+            return ClientWeatherChecker.isThunderAt(clientLevel, ((Panda) (Object) this).blockPosition());
         return original.call(instance);
     }
 }
