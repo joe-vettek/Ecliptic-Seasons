71018bb67e30a5823663127f467ed1871fa2084b
joe-vettek <my1233215@126.com>
2024-09-18 00:56:07 +0800

    add

--- src/main/java/com/teamtea/eclipticseasons/client/particle/ButterflyParticle.java
+++ src/main/java/com/teamtea/eclipticseasons/client/particle/ButterflyParticle.java
@@ -1,6 +1,7 @@
 package com.teamtea.eclipticseasons.client.particle;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.teamtea.eclipticseasons.EclipticSeasonsMod;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.ParticleRenderType;
@@ -48,32 +49,35 @@ public class ButterflyParticle extends FireflyParticle {
         float v0 = this.getV0();
         float v1 = this.getV1();
         int i = this.getLightColor(pPartialTicks);
-        i = 15728880;
-        if (this.age >= this.lifetime * 0.8) {
-            i = this.getLightColor(pPartialTicks);
-        }
+        // i = 15728880;
+        // if (this.age >= this.lifetime * 0.8) {
+        //     i = this.getLightColor(pPartialTicks);
+        // }
 
         float ff = System.currentTimeMillis() % 4000;
 
-        ff = (ff - 2000) / 2000f;
+        ff =  1- (Math.abs((ff - 2000) / 2000f));
+
 
         // pQuaternion=pQuaternion.rotateXYZ(0,0,-Mth.DEG_TO_RAD*90);
         // 主要问题在于枢纽点，后续更换纹理需要更新xoffse和yoffeset
         // pQuaternion = pQuaternion.rotateAxis(ff*45*Mth.DEG_TO_RAD,1,0,0);
 
-        float pXOffset1 = .65f;
-        float pXOffset0 = -0.35f;
-        float pYOffset1 = .65f;
-        float pYOffset0 = -0.35f;
-        pQuaternion=new Quaternionf();
+        float pXOffset1 = 1.f;
+        float pXOffset0 = -1.f;
+        float pYOffset1 = 1.f;
+        float pYOffset0 = -1.f;
+        // pQuaternion=new Quaternionf();
 
-        pQuaternion = pQuaternion.rotateAxis(ff * 45 * Mth.DEG_TO_RAD, 1, 0, 0);
+
+        pQuaternion = pQuaternion.rotateAxis(ff * 70 * Mth.DEG_TO_RAD, 1, 1, 0);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset1, pYOffset0, f, u1, v1, i, 1f);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset1, pYOffset1, f, u1, v0, i, 1f);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset0, pYOffset1, f, u0, v0, i, 1f);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset0, pYOffset0, f, u0, v1, i, 1f);
 
-        pQuaternion = pQuaternion.rotateAxis(ff * -90 * Mth.DEG_TO_RAD, 1, 0, 0);
+
+        pQuaternion = pQuaternion.rotateAxis(ff * -140 * Mth.DEG_TO_RAD, 1, 1, 0);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset1, pYOffset0, f, u1, v1, i, 1f);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset1, pYOffset1, f, u1, v0, i, 1f);
         this.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset0, pYOffset1, f, u0, v0, i, 1f);
@@ -84,6 +88,17 @@ public class ButterflyParticle extends FireflyParticle {
     @Override
     protected void renderVertex(VertexConsumer pBuffer, Quaternionf pQuaternion, float pX, float pY, float pZ, float pXOffset, float pYOffset, float pQuadSize, float pU, float pV, int pPackedLight, float alpha) {
         super.renderVertex(pBuffer, pQuaternion, pX, pY, pZ, pXOffset, pYOffset, pQuadSize, pU, pV, pPackedLight, alpha);
+        // Vector3f vector3f = new Vector3f(pXOffset, pYOffset, 0.0F).rotate(pQuaternion).mul(pQuadSize).add(pX, pY, pZ);
+        // pBuffer.addVertex(vector3f.x(), vector3f.y(), vector3f.z())
+        //         .setUv(pU, pV)
+        //         .setColor(this.rCol, this.gCol, this.bCol, alpha)
+        //         .setNormal(0, 0, 1F)
+        //         .setLight(pPackedLight);
+        // pBuffer.addVertex(vector3f.x(), vector3f.y(), vector3f.z())
+        //         .setUv(pU, pV)
+        //         .setColor(this.rCol, this.gCol, this.bCol, alpha)
+        //         .setNormal(0, 0, -1F)
+        //         .setLight(pPackedLight);
     }
 
     @Override
