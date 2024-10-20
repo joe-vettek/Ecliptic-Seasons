20bf60c68f7f101d3766e26ca491c6d33f25f778
joe-vettek <my1233215@126.com>
2024-10-20 20:14:01 +0800

    add  rubidium support

---
+++ src/main/java/com/teamtea/eclipticseasons/mixin/compat/rubidium/MixinBlockRender2.java
@@ -0,0 +1,60 @@
+package com.teamtea.eclipticseasons.mixin.compat.rubidium;
+
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.teamtea.eclipticseasons.client.core.ModelManager;
+import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.List;
+
+@Mixin({BlockRenderer.class})
+public abstract class MixinBlockRender2 {
+
+
+    @ModifyExpressionValue(
+            remap = false,
+            method = "renderModel",
+            at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/resources/model/BakedModel;getQuads(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Lnet/minecraft/util/RandomSource;Lnet/minecraftforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)Ljava/util/List;")
+    )
+    private List<BakedQuad> mixin_tesselateWithAO_getQuads(List<BakedQuad> original,
+                                                           @Local(argsOnly = true) BlockAndTintGetter world,
+                                                           @Local(argsOnly = true) BlockState blockState,
+                                                           @Local(ordinal = 0, argsOnly = true) BlockPos blockPos,
+                                                           @Local Direction direction,
+                                                           @Local(argsOnly = true) RandomSource randomSource,
+                                                           @Local(argsOnly = true) long seed) {
+        return ModelManager.appendOverlay(world, blockState, blockPos, direction, randomSource, seed, original);
+    }
+
+    @ModifyExpressionValue(
+            remap = false,
+            method = "renderModel",
+            at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/resources/model/BakedModel;getQuads(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/Direction;Lnet/minecraft/util/RandomSource;Lnet/minecraftforge/client/model/data/ModelData;Lnet/minecraft/client/renderer/RenderType;)Ljava/util/List;")
+    )
+    private List<BakedQuad> mixin_tesselateWithAO_getQuads2(List<BakedQuad> original,
+                                                            @Local(argsOnly = true) BlockAndTintGetter world,
+                                                            @Local(argsOnly = true) BlockState blockState,
+                                                            @Local(ordinal = 0, argsOnly = true) BlockPos blockPos,
+                                                            @Local(argsOnly = true) RandomSource randomSource,
+                                                            @Local(argsOnly = true) long seed) {
+        return ModelManager.appendOverlay(world, blockState, blockPos, null, randomSource, seed, original);
+    }
+
+    // @WrapOperation(
+    //         remap = false,
+    //         method = "renderModel",
+    //         at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderer;isFaceVisible(Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderContext;Lnet/minecraft/core/Direction;)Z")
+    // )
+    // private boolean mixin$renderModel_isFaceVisible(BlockRenderer blockRenderer, BlockRenderContext ctx, Direction face, Operation<Boolean> original) {
+    //     return ModelManager.shouldisFaceVisible(blockRenderer,ctx,face,original);
+    // }
+}
